a45c00038d272cb9ec113a9ad9e02cae537586b3
Huy TRAN <huy.tran@lundimatin.fr>
2024-08-19 15:39:45 +0700

    demo rest framework

--- api/views.py
+++ api/views.py
@@ -1,3 +1,73 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import status, generics, permissions
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from api.models import Offer
+from api.permissions import IsAuthorOrReadOnly
+from api.serializers import OfferSerializer, UserSerializer
 
-# Create your views here.
+
+@api_view(['GET', 'POST'])
+def offer_list(request):
+    if request.method == 'GET':
+        offers = Offer.objects.all()
+        serializer = OfferSerializer(offers, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = OfferSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def offer_detail(request, pk):
+    try:
+        offer = Offer.objects.get(pk=pk)
+    except Offer.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = OfferSerializer(offer)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = OfferSerializer(offer, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        offer.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetails(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class OfferList(generics.ListCreateAPIView):
+    queryset = Offer.objects.all()
+    serializer_class = OfferSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class OfferDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Offer.objects.all()
+    serializer_class = OfferSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,
+        IsAuthorOrReadOnly
+    ]
